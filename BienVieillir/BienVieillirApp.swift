875a1af9248dc00231bec173c75e813b8daf1887
Guillaume Richard <guitch95@gmail.com>
2026-06-10 15:06:26 +0200

    Initial commit

---
+++ BienVieillir/BienVieillirApp.swift
@@ -0,0 +1,17 @@
+//
+//  BienVieillirApp.swift
+//  BienVieillir
+//
+//  Created by Guillaume Richard on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BienVieillirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BienVieillir/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BienVieillir
+//
+//  Created by Guillaume Richard on 10/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
