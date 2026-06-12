0b9ce88d1b29521d93c1806499326e873e2cb399
Issa Jaguraga <issa.jaguraga@oclock.school>
2026-06-12 14:42:59 +0200

    feat: improve card layout alignment and structure in conseils view

--- BienVieillir/conseilsView.swift
+++ BienVieillir/conseilsView.swift
@@ -11,60 +11,59 @@ struct conseilsView: View {
     @Environment(ViewModel.self) private var vm
     
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 12){
+            
             Text("Nos Conseils")
                 .font(.title.bold())
             
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(alignment: .top, spacing: 12) {
                     ForEach(vm.arrOfConseil) { conseil in
                         VStack(alignment: .leading) {
-                            
+                            Text(conseil.title)
+                                .font(.headline)
+                                .frame(height: 60)
+                               
                             Text(conseil.description)
-                                .frame(width: 300, height: 250)
-                                .padding()
-                                .background(.background.secondary)
-                                .clipShape(.rect(cornerRadius: 25))
-                                .lineSpacing(5)
-                                .overlay(alignment: .topLeading) {
-                                    Text(conseil.title)
-                                        .padding()
-                                        .font(.headline)
-                                }
+                                .lineSpacing(8)
+                            Spacer()
                         }
+                        .frame(width: 200, height: 250)
+                        .padding()
+                        .background(.background.secondary)
+                        .clipShape(.rect(cornerRadius: 25))
+
                     }
                 }
             }
-            Spacer()
-            
+            .padding(.bottom)
+           
             Text("Nos Ressources")
                 .font(.title.bold())
             
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(alignment: .top, spacing: 12) {
                     ForEach(vm.arrOfActivity) { activite in
                         VStack(alignment: .leading) {
-                    
+                            Text(activite.name)
+                                .font(.headline)
+                                .frame(height: 60)
+                            
                             Text(activite.description)
-                                .frame(width: 300, height: 250)
-                                .padding()
-                                .background(.background.secondary)
-                                .clipShape(.rect(cornerRadius: 25))
-                                .lineSpacing(5)
-                                .overlay(alignment: .topLeading) {
-                                    Text(activite.name)
-                                        .padding()
-                                        .font(.headline)
-                                    
-                                }
+                                .lineSpacing(8)
+                            Spacer()
                         }
+                        .frame(width: 200, height: 250)
+                        .padding()
+                        .background(.background.secondary)
+                        .clipShape(.rect(cornerRadius: 25))
                     }
                 }
             }
         }
         .padding()
+        Spacer()
     }
-        
 }
 #Preview {
     conseilsView()
