5356c7e904a3332aab1cdea9422ca4ed9c14b178
Guillaume Richard <guitch95@gmail.com>
2026-06-14 15:40:24 +0200

    added ConseilsView

---
+++ BienVieillir/ConseilsView.swift
@@ -0,0 +1,18 @@
+//
+//  ConseilsView.swift
+//  BienVieillir
+//
+//  Created by Guillaume Richard on 14/06/2026.
+//
+
+import SwiftUI
+
+struct ConseilsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConseilsView()
+}
