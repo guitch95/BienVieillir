0defa568d6ba1ffa72dd8c4d3333273bc50fbec0
Issa Jaguraga <issa.jaguraga@oclock.school>
2026-06-14 16:28:56 +0200

    Add landing page and update tab bar

--- BienVieillir/ContentView.swift
+++ BienVieillir/ContentView.swift
@@ -1,31 +1,31 @@
-//
-//  ContentView.swift
-//  BienVieillir
-//
-//  Created by Guillaume Richard on 10/06/2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showWelcome: Bool = true
+
     var body: some View {
-        TabView {
-            Tab("Suivi", systemImage: "chart.bar.fill") {
-                ActivityView()
+        if showWelcome {
+            WelcomeView(showWelcome: $showWelcome)
+        } else {
+            TabView {
+
+                Tab("Suivi", systemImage: "chart.line.uptrend.xyaxis.circle.fill") {
+                    ActivityView()
+                }
+
+                Tab("Activités", systemImage: "star.circle") {
+                    ActivityDetailsView()
+                }
+
+                Tab("Détente", systemImage: "apple.meditate.circle.fill") {
+                    DetenteView()
+                }
+
+                Tab("Conseils", systemImage: "lightbulb.fill") {
+                    ConseilsView()
+                }
             }
-            
-            Tab("Activités", systemImage: "star.circle") {
-                ActivityDetailsView()
-            }
-            
-            Tab("Détente", systemImage: "apple.meditate.circle.fill") {
-                DetenteView()
-            }
-            
-            Tab("Conseils", systemImage: "lightbulb.fill") {
-                ConseilsView()
-            }
-            
+            .tint(Color.sauge)
         }
     }
 }
